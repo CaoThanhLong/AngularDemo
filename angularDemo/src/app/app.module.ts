cff4e78e85697d14366760afd38532b8e056a58e
CaoThanhLong <ctlong26051993@gmail.com>
2020-09-22 21:50:09 +0700

    buoi0_tao_component

--- angularDemo/src/app/app.module.ts
+++ angularDemo/src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent, // Cac component su dung (lan nhau) trong module nay
   ],
   imports: [
-    BrowserModule
+    BrowserModule, // Noi de import cac module khac vao module nay
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent], // Noi chua component su dung trong index.html
 })
-export class AppModule { }
+export class AppModule {}
